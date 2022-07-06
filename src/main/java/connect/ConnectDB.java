f2a293ad568e676d0487ad3b1f72ac9927c08532
hauhc1203 <hau.hc1203@gmail.com>
2022-07-06 10:53:21 +0700

    hau commit

---
+++ src/main/java/connect/ConnectDB.java
@@ -0,0 +1,25 @@
+package connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+
+    public static Connection getConnect(){
+        String jdbcURL = "jdbc:mysql://localhost:3306/webBanHang?useSSL=false";
+        String jdbcUsername = "root";
+        String jdbcPassword = "hauhc1203";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection= DriverManager.getConnection(jdbcURL,jdbcUsername,jdbcPassword);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
---
+++ src/main/java/dao/CategoryDAO.java
@@ -0,0 +1,92 @@
+package dao;
+
+import connect.ConnectDB;
+import model.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CategoryDAO implements IDAO<Category> {
+    private static final String INSERT_CATEGORY = "INSERT INTO loaiSanPham (nameCategory) VALUES (?);";
+    private static final String SELECT_ALL_CATEGORY = "select * from loaiSanPham;";
+    private static final String SEARCH_CATEGORY = "select * from loaiSanPham where nameCategory like ? ;";
+    private static final String DELETE_CATEGORY = "delete from loaiSanPham where idCategory = ?;";
+    private static final String SELECT_CATEGORY = "select * from loaiSanPham  where idCategory = ?;";
+
+    private  static  final String UPDATE_CATEGORY="UPDATE loaiSanPham SET nameCategory=?;";
+
+
+
+    @Override
+    public ArrayList<Category> selectAll() {
+        ArrayList<Category> categories =new ArrayList<>();
+        try (Connection connection= ConnectDB.getConnect(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_CATEGORY)){
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                int id=rs.getInt("idCategory");
+                String name = rs.getString("nameCategory");
+                int productQuantity = rs.getInt("productQuantity");
+
+                categories.add(new Category(id,name,productQuantity));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
+    }
+
+    @Override
+    public boolean insert(Category category) {
+        try (Connection connection= ConnectDB.getConnect(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CATEGORY)){
+            preparedStatement.setString(1,category.getNameCategory());
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteByID(int id) {
+        try (Connection connection= ConnectDB.getConnect(); PreparedStatement preparedStatement = connection.prepareStatement(DELETE_CATEGORY)){
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+        }
+        return false;
+    }
+
+    @Override
+    public Category findCByID(int id) {
+
+        try (Connection connection= ConnectDB.getConnect(); PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CATEGORY)){
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet= preparedStatement.executeQuery();
+            resultSet.next();
+            int idCategory=resultSet.getInt("idCategory");
+            String name = resultSet.getString("nameCategory");
+            int productQuantity = resultSet.getInt("productQuantity");
+
+            return new Category(idCategory,name,productQuantity);
+        } catch (SQLException e) {
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean edit(Category category) {
+        try (Connection connection= ConnectDB.getConnect(); PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_CATEGORY)){
+            preparedStatement.setString(1,category.getNameCategory());
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+        }
+        return false;
+    }
+}
